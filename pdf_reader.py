17408d4cbd366c871f7728feb248ff2b3a88926b
shahbuland <smatiana1@gmail.com>
2024-03-06 11:44:38 -0500

    Finalize pdf reader

--- pdf_reader.py
+++ pdf_reader.py
@@ -2,8 +2,7 @@ import os
 import requests
 from marker.convert import convert_single_pdf
 from marker.models import load_all_models
-
-# Assumes marker is setup under "marker"
+from tinygrad.helpers import Timing
 
 # PDF -> Markdown
 class PDFReader:
@@ -11,11 +10,16 @@ class PDFReader:
         self.cache_folder = cache_folder
         self.parallel_factor = parallel_factor
 
+        os.makedirs(self.cache_folder, exist_ok=True)
+
     def __call__(self, url):
-        def download_pdf(self, url):
+        def download_pdf(url):
             # Extract the file name from the URL
             file_name = url.split('/')[-1].replace('.', '-')
             file_path = os.path.join(self.cache_folder, file_name)
+
+            if file_path.endswith("-pdf"):
+                file_path = file_path[:-4] + ".pdf"
 
             # Check if the file already exists in the cache
             if not os.path.exists(file_path):
@@ -28,13 +32,15 @@ class PDFReader:
         if not url.endswith(".pdf"):
             url += ".pdf"
         if not "arxiv" in url:
-            url = "https://arxiv.org/abs/" + url
+            url = "https://arxiv.org/pdf/" + url
         pdf_path = download_pdf(url)
-        full_text, _ = convert_single_pdf(pdf_path, model_lst(), max_pages = 20, parallel_factor = self.parallel_factor)
+        model_lst = load_all_models()
+        full_text, _ = convert_single_pdf(pdf_path, model_lst, max_pages = 20, parallel_factor = self.parallel_factor)
         return full_text
 
 if __name__ == "__main__":
     # Test with the transformers paper
-    url = "https://arxiv.org/abs/1706.03762"
+    url = "1706.03762"
     reader = PDFReader()
-    print(reader(url)[:100])
+    with Timing():
+        output = reader(url)
